f37c85c00bf333a171992922c3155330c88accb7
uoyniXnaH <b4baker@gmail.com>
2021-09-13 08:13:47 +0900

    add changes so-far

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(false);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [scanned, setScanned] = useState(false);
+
+  const handleScanned = ({type, data}) => {
+    setScanned(true);
+    alert(`Get: ${data}`);
+  }
 
   useEffect(() => {
     (async () => {
@@ -22,18 +28,22 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        onBarCodeScanned={scanned ? undefined : handleScanned}
+      >
+        <Text>0.0.6</Text>
       </Camera>
+      {scanned && <Button title={'Try again'} onPress={()=>{setScanned(false)}} />}
+      {!scanned && <Button title={'waiting...'} onPress={()=>alert("waiting...")} />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
   camera: {
     flex: 1
