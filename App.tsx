0a77ff81bdfa23968f44d9803ad82e0bebd0cd30
uoyniXnaH <b4baker@gmail.com>
2021-09-12 11:40:27 +0900

    add camera

--- App.tsx
+++ App.tsx
@@ -1,22 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-function DatePicker() {
-  return(
-    React.createElement("input",
-    {
-      type: "date",
-      name: "target"
-    })
-  )
-}
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Sample text</Text>
-      <DatePicker />
+      <Camera style={styles.camera} type={type}>
+      </Camera>
     </View>
   );
 }
@@ -28,4 +35,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1
+  }
 });
