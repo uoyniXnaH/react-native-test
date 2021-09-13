bd3d02f6d549e0b3a0d64dbda5b3defebc02d42b
uoyniXnaH <b4baker@gmail.com>
2021-09-14 08:19:49 +0900

    changes for using react-qr-reader

--- App.tsx
+++ App.tsx
@@ -1,51 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Camera } from 'expo-camera';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import QrReader from 'react-qr-reader';
+
+function QrReaderComp() {
+  const handleScan = (data: string | null) => {
+    alert('Got: ' + data);
+  };
+
+  const handleError = ({ err }) => {
+    alert('Error: ' + err);
+  };
+  return(
+    React.createElement('QrReader',
+    {
+      delay: 300,
+      onError: handleError,
+      onScan: handleScan
+    })
+  )
+}
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [scanned, setScanned] = useState(false);
+  // const handleScan = (data: string | null) => {
+  //   alert('Got: ' + data);
+  // };
 
-  const handleScanned = ({type, data}) => {
-    setScanned(true);
-    alert(`Get: ${data}`);
-  }
+  // const handleError = ({ err }) => {
+  //   alert('Error: ' + err);
+  // };
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
   return (
-    <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={type}
-        onBarCodeScanned={scanned ? undefined : handleScanned}
-      >
-        <Text>0.0.6</Text>
-      </Camera>
-      {scanned && <Button title={'Try again'} onPress={()=>{setScanned(false)}} />}
-      {!scanned && <Button title={'waiting...'} onPress={()=>alert("waiting...")} />}
+    <View style={Styles.container}>
+      <Text>s1</Text>
+      <QrReaderComp />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const Styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   camera: {
-    flex: 1
-  }
+    width: '100%',
+  },
 });
